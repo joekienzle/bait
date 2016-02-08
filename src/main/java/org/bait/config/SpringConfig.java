f9f4ea85ac7387e1a27f120bf331861a317430bc
j.pfeifer <johaennes.pfeifer@gmail.com>
2016-02-08 19:24:09 +0100

    Added config for JpaRepositories and Hibernate to work

--- src/main/java/org/bait/config/SpringConfig.java
+++ src/main/java/org/bait/config/SpringConfig.java
@@ -1,9 +1,59 @@
 package org.bait.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
-@ComponentScan("org.bait")
+@ComponentScan(SpringConfig.PACKAGE_SCAN_PATH)
+@EnableJpaRepositories(basePackages = SpringConfig.JPA_PACKAGE_PATH)
+@EnableTransactionManagement
 public class SpringConfig {
+
+    public static final String PACKAGE_SCAN_PATH = "org.bait";
+
+    public static final String JPA_PACKAGE_PATH = "org.bait";
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
+        return embeddedDatabaseBuilder.setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setJpaVendorAdapter(createHibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan(JPA_PACKAGE_PATH);
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.afterPropertiesSet();
+
+        return entityManagerFactoryBean.getObject();
+    }
+
+    private HibernateJpaVendorAdapter createHibernateJpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setShowSql(true);
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory());
+        return jpaTransactionManager;
+    }
 }
