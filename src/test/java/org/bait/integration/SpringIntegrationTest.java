e35985816dd1eda0f0d349344e66670284579f46
j.pfeifer <johaennes.pfeifer@gmail.com>
2016-02-08 19:49:56 +0100

    Add overview to pom

--- src/test/java/org/bait/integration/SpringIntegrationTest.java
+++ src/test/java/org/bait/integration/SpringIntegrationTest.java
@@ -2,35 +2,32 @@ package org.bait.integration;
 
 import org.bait.config.SpringConfig;
 import org.bait.model.Bai;
-import org.bait.rest.BaitResource;
+import org.bait.service.BaiService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.ws.rs.core.Response;
-
 import static junit.framework.TestCase.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringConfig.class})
 public class SpringIntegrationTest {
 
     @Autowired
-    private BaitResource baitResource;
+    private BaiService baiService;
 
     @Test
     public void writeTest() {
-        Bai baiMock = mock(Bai.class);
-        when(baiMock.getAccountNumber()).thenReturn("123456");
-        when(baiMock.getBankNumber()).thenReturn("7890");
-        when(baiMock.getBankName()).thenReturn("my bank name");
-        Response response = baitResource.createBaiInfo(baiMock);
-        assertNotNull(response);
-        Bai bai = response.readEntity(Bai.class);
-        assertNotNull(bai);
+        Bai sendBai = new Bai();
+        sendBai.setAccountNumber("123456");
+        sendBai.setBankNumber("7890");
+        sendBai.setBankName("my bank name");
+        Bai persistedBai = baiService.createBankAccountInformation(sendBai);
+        assertNotNull(persistedBai);
+        String baiId = persistedBai.getBaiId();
+        assertNotNull(baiId);
+        System.out.println(baiId);
     }
 }
